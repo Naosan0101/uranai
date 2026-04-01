72a8b42721dbc878e6eaa0304cf2f2826d0aa004
3030913 <3030913@30309-13>
2026-04-01 13:06:05 +0900

    コミット

---
+++ src/main/java/com/example/demo/UranaiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UranaiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UranaiTestApplication.class, args);
+	}
+
+}
